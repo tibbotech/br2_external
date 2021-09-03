25ad70d54a0a9459a67ad53cdaa4b4682602f635
Dvorkin Dmitry <dvorkin@tibbo.com>
2021-09-03 14:40:02 +0300

    update xboot refs

--- br.LTPPxG2/boot/xboot/xboot.mk
+++ br.LTPPxG2/boot/xboot/xboot.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-XBOOT_VERSION = b2210ec08e53432c2d9f29b7dbed5e7ddcb56959
+XBOOT_VERSION = 2ff5ece9eac4b3e6e60523fcc87e30b025c7c804
 XBOOT_SITE = $(call github,tibbotech,plus1_xboot,$(XBOOT_VERSION))
 XBOOT_LICENSE = GPL-2.0
 XBOOT_INSTALL_IMAGES = YES
 XBOOT_INSTALL_TARGET = NO
 
-XBOOT_DRAM_VERSION = 31a770b7163b6ccf52545e2f0440c7fd68615ddd
+XBOOT_DRAM_VERSION = 42936878d6d632103e5ce94c2740b5c827d3eddc
 XBOOT_EXTRA_DOWNLOADS = $(call github,tibbotech,plus1_draminit,$(XBOOT_DRAM_VERSION))/dram-$(XBOOT_DRAM_VERSION).tar.gz
  
 define XBOOT_EXTRACT_DRAM
