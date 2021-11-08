27c2f5326b3d6f2383060e634eeb09f1d7c8fc33
Dvorkin Dmitry <dvorkin@tibbo.com>
2021-11-09 00:56:13 +0300

    spmake: set variables if no defined in ENV. Fix _xhdr path

--- br.LTPPxG2/package/spmake/files/sp_make.inc.mk
+++ br.LTPPxG2/package/spmake/files/sp_make.inc.mk
@@ -14,35 +14,34 @@ OUTD=sp_out
 ###### rootfs : there are several of them
 
 ###### *** Xboot (arm926,Chip-B): pre-uboot, bootloader #1
+F_XBT ?= xboot
+#F_XBT=xboot-emmc
 #F_XBT=xboot-nand
-F_XBT=xboot-emmc
-F_XBT=xboot
 
 ###### xxx No-OS (arm926,Chip-B): main binary [optional]
-F_NON=a926
 # a926 firmware ipc_* makes emmc unstable now
-F_NON=a926-empty
+F_NON ?= a926-empty
+#F_NON=a926
 
 ###### *** U-Boot (A7,Chip-A): bootloader #2
+F_UBT ?= u-boot.bin
 #F_UBT=u-boot-a7021_emmc-sp-r0.img
 #F_UBT=u-boot.img-a7021_nand
 #F_UBT=u-boot.bin-a7021_dsxe
-F_UBT=u-boot.bin-a7021_ppg2
-F_UBT=u-boot.bin
+#F_UBT=u-boot.bin-a7021_ppg2
 
 ###### *** DTS
+F_DTB ?= sp7021-ltpp3g2revD.dtb
 #F_DTB=sp7021-ev.dtb
 #F_DTB=sp7021-dsx.dtb
-F_DTB=sp7021-ltpp3g2revD.dtb
 
 ###### xxx Kernel (A7,Chip-A): Linux kernel
-F_KRN=uImage
-# use kernel with initrd for FS repair functions
-F_KRN=uImage-initramfs-tppg2
-F_KRN=uImage
+F_KRN ?= uImage
+# use kernel with initrd for FS repair functions (EMMC)
+#F_KRN=uImage-initramfs-tppg2
 
 ###### *** rootfs
+F_ROO ?= rootfs.ext4
 #F_ROO=img-sp-tiny-tppg2.ext4
-F_ROO=img-tps-base-tppg2.ext4
+#F_ROO=img-tps-base-tppg2.ext4
 #F_ROO=img-tps-free-tppg2.ext4
-F_ROO=rootfs.ext4
